52a9cbbb11eb05d9e8fa032083bac49b1eec6a57
shasha56 <121288332+shasha56@users.noreply.github.com>
2025-12-04 22:49:07 +0900

    first commit

---
+++ date.js
@@ -0,0 +1,74 @@
+export const date = [
+    {
+        class: 'A101',
+        Monday: '0000000',
+        Tuesday: '1010100',
+        Wednesday: '1101101',
+        Thrusday: '0010000',
+        Friday: '1101100'
+    },
+    {
+        class: 'A101',
+        Monday: '0000000',
+        Tuesday: '1010100',
+        Wednesday: '1101101',
+        Thrusday: '0010000',
+        Friday: '1101100'
+    },
+    {
+        class: 'A101',
+        Monday: '0000000',
+        Tuesday: '1010100',
+        Wednesday: '1101101',
+        Thrusday: '0010000',
+        Friday: '1101100'
+    },
+    {
+        class: 'A101',
+        Monday: '0000000',
+        Tuesday: '1010100',
+        Wednesday: '1101101',
+        Thrusday: '0010000',
+        Friday: '1101100'
+    },
+    {
+        class: 'A101',
+        Monday: '0000000',
+        Tuesday: '1010100',
+        Wednesday: '1101101',
+        Thrusday: '0010000',
+        Friday: '1101100'
+    },
+    {
+        class: 'A101',
+        Monday: '0000000',
+        Tuesday: '1010100',
+        Wednesday: '1101101',
+        Thrusday: '0010000',
+        Friday: '1101100'
+    },
+    {
+        class: 'A101',
+        Monday: '0000000',
+        Tuesday: '1010100',
+        Wednesday: '1101101',
+        Thrusday: '0010000',
+        Friday: '1101100'
+    },
+    {
+        class: 'A101',
+        Monday: '0000000',
+        Tuesday: '1010100',
+        Wednesday: '1101101',
+        Thrusday: '0010000',
+        Friday: '1101100'
+    },
+    {
+        class: '',
+        Monday: '',
+        Tuesday: '',
+        Wednesday: '',
+        Thrusday: '',
+        Friday: ''
+    },
+]

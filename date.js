490cd59f1bdd44acb56a68f7ffe55e014c74cc4f
any6346 <121288332+shasha56@users.noreply.github.com>
2025-12-19 17:53:48 +0900

    ダミーデータを作成

--- date.js
+++ date.js
@@ -1,83 +1,74 @@
-export const date = [
+const data = [
     {
-        class: 'A101',
-        Monday: '0000000',
-        Tuesday: '1010100',
-        Wednesday: '1101101',
-        Thrusday: '0010000',
-        Friday: '1101100',
-        supplement: 'コンセントなし'
-    },
-    {
-        class: 'A101',
-        Monday: '0000000',
-        Tuesday: '1010100',
-        Wednesday: '1101101',
-        Thrusday: '0010000',
-        Friday: '1101100',
+        class: 'A102',
+        Monday: '0100000',
+        Tuesday: '0000000',
+        Wednesday: '1010100',
+        Thursday: '0000000',
+        Friday: '1100000',
         supplement: 'コンセントあり'
     },
     {
-        class: 'A101',
-        Monday: '0000000',
-        Tuesday: '1010100',
-        Wednesday: '1101101',
-        Thrusday: '0010000',
-        Friday: '1101100',
+        class: 'A103',
+        Monday: '1111111',
+        Tuesday: '1010101',
+        Wednesday: '0000000',
+        Thursday: '1010100',
+        Friday: '0000011',
         supplement: 'コンセントなし'
     },
     {
-        class: 'A101',
-        Monday: '0000000',
-        Tuesday: '1010100',
-        Wednesday: '1101101',
-        Thrusday: '0010000',
-        Friday: '1101100',
-        supplement: 'コンセントなし'
-    },
-    {
-        class: 'A101',
-        Monday: '0000000',
-        Tuesday: '1010100',
-        Wednesday: '1101101',
-        Thrusday: '0010000',
-        Friday: '1101100',
+        class: 'A201',
+        Monday: '0011000',
+        Tuesday: '0000000',
+        Wednesday: '1000100',
+        Thursday: '0000010',
+        Friday: '0000000',
         supplement: 'コンセントあり'
     },
     {
-        class: 'A101',
-        Monday: '0000000',
-        Tuesday: '1010100',
-        Wednesday: '1101101',
-        Thrusday: '0010000',
-        Friday: '1101100',
+        class: 'A205',
+        Monday: '1000000',
+        Tuesday: '0101000',
+        Wednesday: '0001000',
+        Thursday: '1110000',
+        Friday: '1000000',
         supplement: 'コンセントなし'
     },
     {
-        class: 'A101',
+        class: 'A301',
         Monday: '0000000',
-        Tuesday: '1010100',
-        Wednesday: '1101101',
-        Thrusday: '0010000',
-        Friday: '1101100',
-        supplement: 'コンセントなし'
-    },
-    {
-        class: 'A101',
-        Monday: '0000000',
-        Tuesday: '1010100',
-        Wednesday: '1101101',
-        Thrusday: '0010000',
-        Friday: '1101100',
+        Tuesday: '1100110',
+        Wednesday: '0000000',
+        Thursday: '1101101',
+        Friday: '0000000',
         supplement: 'コンセントあり'
     },
     {
-        class: '',
-        Monday: '',
-        Tuesday: '',
-        Wednesday: '',
-        Thrusday: '',
-        Friday: '',
-        supplement: ''
+        class: 'A304',
+        Monday: '0101010',
+        Tuesday: '1010101',
+        Wednesday: '0101010',
+        Thursday: '1010101',
+        Friday: '1110000',
+        supplement: 'コンセントなし'
     },
-]
+    {
+        class: 'A402',
+        Monday: '1110000',
+        Tuesday: '0001110',
+        Wednesday: '1100000',
+        Thursday: '0000011',
+        Friday: '1111000',
+        supplement: 'コンセントあり'
+    },
+    {
+        class: 'A410',
+        Monday: '0000000',
+        Tuesday: '0000000',
+        Wednesday: '0000000',
+        Thursday: '0000000',
+        Friday: '1000000',
+        supplement: 'コンセントなし'
+    }
+];

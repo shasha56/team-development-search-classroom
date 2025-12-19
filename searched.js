7844dd778077644c1cc4404608ab6d0ea637500f
any6346 <121288332+shasha56@users.noreply.github.com>
2025-12-19 18:08:42 +0900

    仮のjsコード完成
    Updated the result function to populate a table with classroom data, including class names and availability status for each day.

--- searched.js
+++ searched.js
@@ -1,8 +1,37 @@
-import {date} from "./date.js"; //date.jsの配列を取得
-const dateLength = date.length; //配列のデータ数
+import {data} from "./data.js"; //data.jsの配列を取得
+const dataLength = data.length; //配列のデータ数
+
+const tbody = document.getElementById('tbody');
+const fragment = document.createDocumentFragment();
 
 function result(){
-    for(let i = 0; i < dateLength; i++){
+    for(let i = 0; i < dataLength; i++){
+        const tr = document.createElement("tr");
+        tr.setAttribute("id","classroom" + String(i));
+        tbody.appendChild(tr);
+        const trNum = document.getElementById("classroom" + String(i));
+
+        const tdClass = document.createElement("td"); //教室名
+        tdClass.textContent = data[i].class;
+        fragment.appendChild(tdClass);
+        trNum.appendChild(fragment);
+
+        for(let j = 0; j < 7; j++){
+            const tdFlag = document.createElement("td"); //n限目の空き状況
+            if(data[i]['Monday'][j] == "0"){
+                tdFlag.textContent = "空";
+            } else {
+                tdFlag.textContent = "使用中";
+            }
+            fragment.appendChild(tdFlag);
+            trNum.appendChild(fragment);
+        }
         
+        const tdSup = document.createElement("td"); //補足
+        tdSup.textContent = data[i].supplement;
+        fragment.appendChild(tdSup);
+        trNum.appendChild(fragment);
     }   
 }
+
+result();
